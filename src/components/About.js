91093fbc44465f2d46a5d0eedcfc5d9bcb0ec7b0
leslied41 <leslied41@gmail.com>
2021-09-22 01:01:34 +1000

    first debug

--- src/components/About.js
+++ src/components/About.js
@@ -34,12 +34,12 @@ export default function About() {
   }
   return (
     <main className="relative">
-      <img src={image} alt="image" className="absolute w-full" />
+      <img src={image} alt="bc" className="absolute w-full" />
       <div className="p-10 lg:pt-48 container ma-auto relative">
         <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
           <img
             src={urlFor(author.authorImage).url()}
-            alt="image"
+            alt="avatar"
             className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
           />
           <div className="text-lg flex flex-col justify-center ">
